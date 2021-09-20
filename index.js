db0c2c27adf214f38c055b3b7fa5f7c0949ad39b
JJ <jc@orbsdigital.com>
2021-09-20 11:16:36 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solutions in this file
+
+//Practice: The for loop
+/*
+for (let age = 30; age < 40; age ++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+}
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+//Assignment
+const names = ["Ada", "Brendan", "Ali"];
+
+function writeCards(names){
+    for(i = 0; i < names.length; i++) {
+        console.log(`"Thank you, ${names[i]}, for the wonderful birthday gift!"`);
+        
+    }
+
+    return names;
+}
+
+writeCards(names);
+*/
+//Practice: The while loop
+
+
+function countDown() {
+    let countDown = 10;
+    while ( countDown > 0 ) {
+        console.log(countDown--);
+    }
+    return countDown;
+    debugger;
+}
+
+countDown(10);
